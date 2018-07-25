46d59d60cfbe964159a7a441e18a311fe0e5a469
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-07-25 12:15:23 +0300

    Update and rename 03.rb to triangle.rb

---
+++ triangle.rb
@@ -0,0 +1,19 @@
+puts "Triangle check"
+
+puts "Please, enter 'a' side:"
+a = gets.to_f
+puts "Please, enter 'b' side:"
+b = gets.to_f
+puts "Please, enter 'c' side:"
+c = gets.to_f
+
+hip = [a,b,c].max
+cat1, cat2 = [a,b,c].min(2)
+rectangular = hip**2 == cat1**2 + cat2**2
+if rectangular && cat1 == cat2
+  puts "Triangle is rectangular and isosceles"
+elsif rectangular 
+  puts "Triangle is rectangular"
+else
+  puts "Triangle is not rectangular"
+end
