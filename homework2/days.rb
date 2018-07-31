af66720582d648de681f3706b884460edd50167b
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-07-31 11:27:48 +0300

    Update days.rb

--- homework2/days.rb
+++ homework2/days.rb
@@ -9,7 +9,7 @@ month = gets.to_i
 puts 'Year:' 
 year = gets.to_i
 
-months[:february] = 29 if (year % 4).zero? || (year % 400).zero?
+months[:february] = 29 if (year % 4).zero? || (year % 400).zero?  && (year % 100 != 0)
 
 result = day
 
