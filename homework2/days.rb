a6d0dee7408851fc9056453f83a207920bb8ee15
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-04 12:02:34 +0300

    Update days.rb

--- homework2/days.rb
+++ homework2/days.rb
@@ -1,35 +1,13 @@
-months = [31,28,31,30,31,30,31,31,30,31,30,31]
-day_count = 0
-count = 0
-count_days_in_months = 0
-i = 0
-print "Enter date: "
-day = gets.chomp.to_i
-
-print "Enter month: "
-month = gets.chomp.to_i
-
-print "Enter year: "
-year = gets.chomp.to_i
-
-if year % 4 == 0 && year % 100 != 0 || year % 400 == 0 
-  months[1] = 29
+puts "Monthes: "
+months = { "january" => 31, "february" => 28, "march" => 31, "april" => 30, 
+  "may" => 31, "june" => 30, "july" => 31, "august" => 31, "september" => 30, 
+  "october" => 31, "november" => 30, "december" => 31 }
+months.each do |m, d|
+  if d%30 == 0
+    puts m    
+  end
 end
 
-if month > 1 
-  count = month - 1
-else
-  puts "This is #{day} day in #{year} year."
-  exit
-end
+puts
 
-count.times do |i|
-    
-  count_days_in_months = count_days_in_months + months[i]
-  i = i + 1
-    
-end
-
-day_count = count_days_in_months + day
-
-puts "This is #{day_count} day."
+months.each {|key, value| puts key if value == 30}
