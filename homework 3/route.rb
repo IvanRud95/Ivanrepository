76a924bb1166c28df149dff3ec786955e5683ac6
i95.rudenko@gmail.com <fAcet7296>
2018-08-12 14:05:07 +0300

    modify homework 3/route.rb

--- homework 3/route.rb
+++ homework 3/route.rb
@@ -1,26 +1,31 @@
 class Route
-  attr_reader :stations, :from, :to
 
-def initialize(from, to)
-    @stations = [from, to]
+  attr_reader :stations
+
+   def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
   end
 
-  def add_station(station)
-    stations.insert(-2, station)
+   def add_station(name_station)
+    stations.insert(-2, name_station)
   end
 
-  def remove_station(station)
-    if [stations.first, stations.last].include?(station)
-      puts "You can't remove first and last stations"
+   def remove_station(name_station)
+    if [stations.first, stations.last].include?(name_station)
     else
-      stations.delete(station)
+      stations.delete(name_station)
     end
   end
 
-  def list_stations
-    puts "The route '#{stations.first.name} - #{stations.last.name}' include stations: "
-    stations.each { |station| puts station.name}
-  end
+   def display_stations
+   @stations.each { |station| puts station.name }
+ end
 
+   def first_station
+   @stations.first
+ end
+
+   def last_station
+   @stations.last
+ end
 end
- 
