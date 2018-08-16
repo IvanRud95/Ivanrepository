8970e65f3334b5b944dce72d9bf2fe4287fc73c0
i95.rudenko@gmail.com <fAcet7296>
2018-08-16 14:21:55 +0300

    new

--- homework 3/route.rb
+++ homework 3/route.rb
@@ -1,31 +1,31 @@
 class Route
-  
-  attr_reader :stations :first :last
 
-  def initialize(first, last)
-   @stations = [first, last]
-   @route = []
- end
+   attr_reader :stations 
+
+   def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+    @route = []
+   end
 
    def add_station(station)
     @stations.insert(-2, station)
- end
+   end
 
    def display_station(station)
     @stations.each { |station| puts station.name }
- end
+   end
 
    def del_station(station)
-   if first_station != station && last_station != station
+     if first_station != station && last_station != station
      @stations.delete(station)
+     end
    end
- end
 
    def first_station
    @stations.first
- end
+   end
 
    def last_station
    @stations.last
- end
-end
+   end
+  end
