8632f93722a90829662c81b0d047e7dd11b77799
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-24 11:21:01 +0300

    Create passenger_train.rb

---
+++ homework 3/passenger_train.rb
@@ -0,0 +1,18 @@
+class PassengerTrain < Train
+  validate :id, :presence
+  validate :id, :type
+  validate :id, :format, ID
+  def add_coach(coach)
+    validate_coach!(coach)
+    super(coach)
+  end
+
+  protected
+
+  def validate_coach!(coach)
+    raise 'coach type error' unless coach.instance_of? PassengerCoach
+    true
+  end
+
+  
+end
