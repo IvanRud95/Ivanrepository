d2af5a21b105f3796d18a6fb40c129b1d54f3270
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-26 14:33:37 +0300

    Create coach.rb

---
+++ homework 3/coach.rb
@@ -0,0 +1,13 @@
+require_relative 'modules/entity_type'
+require_relative 'modules/manufacturer'
+require_relative 'modules/instance_counter'
+
+class Coach
+  include EntityType
+  include Manufacturer
+  include InstanceCounter
+
+  def initialize
+    register_instance
+  end
+end
