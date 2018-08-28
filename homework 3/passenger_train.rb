7f64bbeb3575db504eb0b59690a4e2dd789f8c98
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-28 14:30:59 +0300

    Update passenger_train.rb

--- homework 3/passenger_train.rb
+++ homework 3/passenger_train.rb
@@ -1,17 +1,11 @@
 class PassengerTrain < Train
   validate :id, :presence
-  validate :id, :type
-  validate :id, :format, ID
-  def add_coach(coach)
-    validate_coach!(coach)
-    super(coach)
-  end
 
-  protected
 
-  def validate_coach!(coach)
-    raise 'coach type error' unless coach.instance_of? PassengerCoach
-    true
+
+  def initialize(number, manufacture_name)
+    super(number, manufacture_name)
+    @type = :passenger
   end
 
   
