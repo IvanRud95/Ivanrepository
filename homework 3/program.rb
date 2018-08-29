a09b468f80b0e24afc64b68ed9b4db75becf813e
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-29 16:01:30 +0300

    Update program.rb

--- homework 3/program.rb
+++ homework 3/program.rb
@@ -1,4 +1,12 @@
 require_relative 'main.rb'
+require_relative 'station'
+require_relative 'route'
+require_relative 'train'
+require_relative 'cargo_train'
+require_relative 'passenger_train'
+require_relative 'passenger_wagon'
+require_relative 'passenger_coach'
+
 
 main = Main.new
 main.start
