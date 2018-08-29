03f7591410aedf171dea93bfc07704b8a3600584
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-29 15:44:33 +0300

    Update passenger_coach.rb

--- homework 3/passenger_coach.rb
+++ homework 3/passenger_coach.rb
@@ -1,3 +1,18 @@
 class PassengerCoach < Coach
 
+  attr_reader :total_places
+  attr_reader :occupied_places
+
+  def initialize(total)
+    unless total.is_a?(Integer) && total > 0
+      fail 'Total number of places should be integer bigger then 0.'
+    end
+
+    @total_places    = total
+    @occupied_places = 0
+  end
+
+  def available_places
+    @total_places - @occupied_places
+  end
 end
