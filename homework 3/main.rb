782393b817a31c1ecc84b1392d8b715a92a6a8d4
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-29 22:03:50 +0300

    Update main.rb

--- homework 3/main.rb
+++ homework 3/main.rb
@@ -7,5 +7,8 @@ require_relative 'coach.rb'
 require_relative 'passenger_coach.rb'
 require_relative 'cargo_coach.rb'
 require_relative 'railway_system.rb'
-require_relative 'demo.rb'
-require_relative 'menu_info.rb'
+
+@stations = []
+@trains = []
+@routes = []
+@wagons = []
