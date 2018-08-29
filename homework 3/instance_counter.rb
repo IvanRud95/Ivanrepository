9fad701a3ccf844766dd14af2f8217c0e43a8cbd
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-29 21:57:45 +0300

    Update instance_counter.rb

--- homework 3/instance_counter.rb
+++ homework 3/instance_counter.rb
@@ -1,24 +1,25 @@
 module InstanceCounter
-  def self.included(base)
-    base.extend ClassMethods
-    base.send :include, InstanceMethods
-  end
+   def self.included(base)
+     base.extend ClassMethods
+     base.send :include, InstanceMethods
+   end
 
-  module ClassMethods
-    def instances
-      @instance_counter ||= 0
-    end
+   module ClassMethods
+     def instances
+       @instance_counter ||= 0
+     end
 
-    def instances=(value)
-      @instance_counter = value
-    end
-  end
+     def instances=(value)
+       @instance_counter = value
+     end
+   end
 
-  module InstanceMethods
-    protected
+   module InstanceMethods
+     private
 
     def register_instance
-      self.class.instances += 1
+       self.class.count ||= 0
+       self.class.instances += 1
     end
-  end
+   end
 end
