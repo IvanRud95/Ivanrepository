c7591c53357147989269267e5dcb905837e64b2a
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-08-30 18:49:45 +0300

    Update main.rb

--- homework 3/main.rb
+++ homework 3/main.rb
@@ -3,10 +3,8 @@ require_relative 'route.rb'
 require_relative 'train.rb'
 require_relative 'cargo_train.rb'
 require_relative 'passenger_train.rb'
-require_relative 'coach.rb'
-require_relative 'passenger_coach.rb'
-require_relative 'cargo_coach.rb'
-require_relative 'railway_system.rb'
+require_relative 'passenger_wagon.rb'
+
 
 puts "Lets create Railroad."
 
