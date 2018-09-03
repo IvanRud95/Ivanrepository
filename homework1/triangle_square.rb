cdf210c7b92b6b2cad5e3880fd3cde67e8e2a949
i95.rudenko@gmail.com <fAcet7296>
2018-09-03 12:15:51 +0300

    new"
    exit
    end
    ..
    //qw
    e
    q
    dw
    
    fcfr
    wgerklgh0p2wef3
    g2q
    3w
    r2
    13r1

---
+++ homework1/triangle_square.rb
@@ -0,0 +1,9 @@
+puts "Enter base of the triangle?"
+base = gets.to_f
+
+puts "Enter height of the triangle?"
+height = gets.to_f
+
+result = 0.5 * base * height
+
+puts "The square of triangle is #{result}."
