933a84a8e4644513f44200ed9dcd24a721ce8645
i95.rudenko@gmail.com <fAcet7296>
2018-09-06 11:13:58 +0300

    new

--- homework4/interface.rb
+++ homework4/interface.rb
@@ -1,13 +1,12 @@
 Class Interface
 
-  def main_menu
-    puts "1.Stations"
-    puts "2.Routes"
-    puts "3.Trains"
-    puts "4.Wagons"
-    puts "0.Exit"
-    gets.to_i
-  end
+   def main_menu
+     puts "1.Stations"
+     puts "2.Routes"
+     puts "3.Trains"
+     puts "4.Wagons"
+     puts "0.Exit"
+     gets.to_i
+   end
 
 end
- 
